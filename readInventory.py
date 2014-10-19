09c783894eab71604d286f6845d2575da1ccb975
ReeceStevens <reecestevens@utexas.edu>
2014-10-18 21:08:17 -0500

    Updated inventory read script

--- readInventory.py
+++ readInventory.py
@@ -1,20 +1,41 @@
 import xlrd
 import csv
 import sys
+import magic
+import re
+
+def readInput(inputfile):
+    xlsx_match = re.compile(".*Excel.*"); 
+    xls_match = re.compile("Composite Document.*");
+    if (magic.from_file(inputfile) == 'ASCII text, with CRLF line terminators'):
+        copyfile(inputfile, 'inventory_maintenance_db.csv');
+        parsecsv('inventory_maintenance_db.csv');
+    elif ((xlsx_match.match(magic.from_file(inputfile)) != None) | (xls_match.match(magic.from_file(inputfile)) != None)):
+        csv_conversion = xls2csv(inputfile);
+        parsecsv(csv_conversion); 
+    else:
+        raise IncompatibleFileType;
+        return -1;
+
+def parsecsv(csvfile):
+    with open(csvfile, 'rb') as myFile:
+        # Look for the files we need
+        parser = csv.reader(myFile, delimiter=',', quotechar='"');
+        for row in parser[1:]:
+            # What about header files? Skipping first row for now
+            row.append("No PM done"); # Append the last PM date column to each row
+
+
 
 def xls2csv(xlsfile):
-
     wb = xlrd.open_workbook(xlsfile);
     sh = wb.sheet_by_name('Sheet1');
     csvfile = open('inventory_maintenance_db.csv', 'wb');
     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL);
 
-    for row in range (1, sh.nrows+1):
+    for row in xrange(sh.nrows):
         writer.writerow(sh.row_values(row));
 
     csvfile.close();
-
-if __name__ == "__main__":
-    xls2csv(sys.argv[1]);
-
-
+    return 'inventory_maintenance)db.csv';
+readInput('TEST.xlsx')
